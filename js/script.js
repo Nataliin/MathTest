28839634dc61a74b313103839b3182384403442e
Nataliin <114929195+Nataliin@users.noreply.github.com>
2023-03-29 23:20:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,534 @@
+let button = document.getElementById("finish");
+
+button.addEventListener("click", checkTest);
+
+function checkTest() {
+    let result = 0;
+    
+    let answer1 = document.getElementById("column1-1").value;
+    if(answer1 === "1") {
+        result++;
+    }
+
+    let answer2 = document.getElementById("column1-2").value;
+    if(answer2 === "2") {
+        result++;
+    }
+
+    let answer3 = document.getElementById("column1-3").value;
+    if(answer3 === "3") {
+        result++;
+    }
+
+    let answer4 = document.getElementById("column1-4").value;
+    if(answer4 === "4") {
+        result++;
+    }
+
+    let answer5 = document.getElementById("column1-5").value;
+    if(answer5 === "5") {
+        result++;
+    }
+
+    let answer6 = document.getElementById("column1-6").value;
+    if(answer6 === "6") {
+        result++;
+    }
+
+    let answer7 = document.getElementById("column1-7").value;
+    if(answer7 === "7") {
+        result++;
+    }
+
+    let answer8 = document.getElementById("column1-8").value;
+    if(answer8 === "8") {
+        result++;
+    }
+
+    let answer9 = document.getElementById("column1-9").value;
+    if(answer9 === "9") {
+        result++;
+    }
+
+    let answer10 = document.getElementById("column1-10").value;
+    if(answer10 === "10") {
+        result++;
+    }
+
+
+
+    let answer011 = document.getElementById("column2-1").value;
+    if(answer011 === "2") {
+        result++;
+    }
+
+    let answer012 = document.getElementById("column2-2").value;
+    if(answer012 === "4") {
+        result++;
+    }
+
+    let answer013 = document.getElementById("column2-3").value;
+    if(answer013 === "6") {
+        result++;
+    }
+
+    let answer014 = document.getElementById("column2-4").value;
+    if(answer014 === "8") {
+        result++;
+    }
+
+    let answer015 = document.getElementById("column2-5").value;
+    if(answer015 === "10") {
+        result++;
+    }
+
+    let answer016 = document.getElementById("column2-6").value;
+    if(answer016 === "12") {
+        result++;
+    }
+
+    let answer017 = document.getElementById("column2-7").value;
+    if(answer017 === "14") {
+        result++;
+    }
+
+    let answer018 = document.getElementById("column2-8").value;
+    if(answer018 === "16") {
+        result++;
+    }
+
+    let answer019 = document.getElementById("column2-9").value;
+    if(answer019 === "18") {
+        result++;
+    }
+
+    let answer020 = document.getElementById("column2-10").value;
+    if(answer020 === "20") {
+        result++;
+    }
+
+
+
+
+
+    let answer21 = document.getElementById("column3-1").value;
+    if(answer21 === "3") {
+        result++;
+    }
+
+    let answer22 = document.getElementById("column3-2").value;
+    if(answer22 === "6") {
+        result++;
+    }
+
+    let answer23 = document.getElementById("column3-3").value;
+    if(answer23 === "9") {
+        result++;
+    }
+
+    let answer24 = document.getElementById("column3-4").value;
+    if(answer24 === "12") {
+        result++;
+    }
+
+    let answer25 = document.getElementById("column3-5").value;
+    if(answer25 === "15") {
+        result++;
+    }
+
+    let answer26 = document.getElementById("column3-6").value;
+    if(answer26 === "18") {
+        result++;
+    }
+
+    let answer27 = document.getElementById("column3-7").value;
+    if(answer27 === "21") {
+        result++;
+    }
+
+    let answer28 = document.getElementById("column3-8").value;
+    if(answer28 === "24") {
+        result++;
+    }
+
+    let answer29 = document.getElementById("column3-9").value;
+    if(answer29 === "27") {
+        result++;
+    }
+
+    let answer30 = document.getElementById("column3-10").value;
+    if(answer30 === "30") {
+        result++;
+    }
+
+
+    let answer31 = document.getElementById("column4-1").value;
+    if(answer31 === "4") {
+        result++;
+    }
+
+    let answer32 = document.getElementById("column4-2").value;
+    if(answer32 === "8") {
+        result++;
+    }
+
+    let answer33 = document.getElementById("column4-3").value;
+    if(answer33 === "12") {
+        result++;
+    }
+
+    let answer34 = document.getElementById("column4-4").value;
+    if(answer34 === "16") {
+        result++;
+    }
+
+    let answer35 = document.getElementById("column4-5").value;
+    if(answer35 === "20") {
+        result++;
+    }
+
+    let answer36 = document.getElementById("column4-6").value;
+    if(answer36 === "24") {
+        result++;
+    }
+
+    let answer37 = document.getElementById("column4-7").value;
+    if(answer37 === "28") {
+        result++;
+    }
+
+    let answer38 = document.getElementById("column4-8").value;
+    if(answer38 === "32") {
+        result++;
+    }
+
+    let answer39 = document.getElementById("column4-9").value;
+    if(answer39 === "36") {
+        result++;
+    }
+
+    let answer40 = document.getElementById("column4-10").value;
+    if(answer40 === "40") {
+        result++;
+    }
+
+
+
+
+    let answer41 = document.getElementById("column5-1").value;
+    if(answer41 === "5") {
+        result++;
+    }
+
+    let answer42 = document.getElementById("column5-2").value;
+    if(answer42 === "10") {
+        result++;
+    }
+
+    let answer43 = document.getElementById("column5-3").value;
+    if(answer43 === "15") {
+        result++;
+    }
+
+    let answer44 = document.getElementById("column5-4").value;
+    if(answer44 === "20") {
+        result++;
+    }
+
+    let answer45 = document.getElementById("column5-5").value;
+    if(answer45 === "25") {
+        result++;
+    }
+
+    let answer46 = document.getElementById("column5-6").value;
+    if(answer46 === "30") {
+        result++;
+    }
+
+    let answer47 = document.getElementById("column5-7").value;
+    if(answer47 === "35") {
+        result++;
+    }
+
+    let answer48 = document.getElementById("column5-8").value;
+    if(answer48 === "40") {
+        result++;
+    }
+
+    let answer49 = document.getElementById("column5-9").value;
+    if(answer49 === "45") {
+        result++;
+    }
+
+    let answer50 = document.getElementById("column5-10").value;
+    if(answer50 === "50") {
+        result++;
+    }
+
+
+
+
+    let answer51 = document.getElementById("column6-1").value;
+    if(answer51 === "6") {
+        result++;
+    }
+
+    let answer52 = document.getElementById("column6-2").value;
+    if(answer52 === "12") {
+        result++;
+    }
+
+    let answer53 = document.getElementById("column6-3").value;
+    if(answer53 === "18") {
+        result++;
+    }
+
+    let answer54 = document.getElementById("column6-4").value;
+    if(answer54 === "24") {
+        result++;
+    }
+
+    let answer55 = document.getElementById("column6-5").value;
+    if(answer55 === "30") {
+        result++;
+    }
+
+    let answer56 = document.getElementById("column6-6").value;
+    if(answer56 === "36") {
+        result++;
+    }
+
+    let answer57 = document.getElementById("column6-7").value;
+    if(answer57 === "42") {
+        result++;
+    }
+
+    let answer58 = document.getElementById("column6-8").value;
+    if(answer58 === "48") {
+        result++;
+    }
+
+    let answer59 = document.getElementById("column6-9").value;
+    if(answer59 === "54") {
+        result++;
+    }
+
+    let answer60 = document.getElementById("column6-10").value;
+    if(answer60 === "60") {
+        result++;
+    }
+
+
+
+
+    let answer61 = document.getElementById("column7-1").value;
+    if(answer61 === "7") {
+        result++;
+    }
+
+    let answer62 = document.getElementById("column7-2").value;
+    if(answer62 === "14") {
+        result++;
+    }
+
+    let answer63 = document.getElementById("column7-3").value;
+    if(answer63 === "21") {
+        result++;
+    }
+
+    let answer64 = document.getElementById("column7-4").value;
+    if(answer64 === "28") {
+        result++;
+    }
+
+    let answer65 = document.getElementById("column7-5").value;
+    if(answer65 === "35") {
+        result++;
+    }
+
+    let answer66 = document.getElementById("column7-6").value;
+    if(answer66 === "42") {
+        result++;
+    }
+
+    let answer67 = document.getElementById("column7-7").value;
+    if(answer67 === "49") {
+        result++;
+    }
+
+    let answer68 = document.getElementById("column7-8").value;
+    if(answer68 === "56") {
+        result++;
+    }
+
+    let answer69 = document.getElementById("column7-9").value;
+    if(answer69 === "63") {
+        result++;
+    }
+
+    let answer70 = document.getElementById("column7-10").value;
+    if(answer70 === "70") {
+        result++;
+    }
+
+
+
+
+    let answer71 = document.getElementById("column8-1").value;
+    if(answer71 === "8") {
+        result++;
+    }
+
+    let answer72 = document.getElementById("column8-2").value;
+    if(answer72 === "16") {
+        result++;
+    }
+
+    let answer73 = document.getElementById("column8-3").value;
+    if(answer73 === "24") {
+        result++;
+    }
+
+    let answer74 = document.getElementById("column8-4").value;
+    if(answer74 === "32") {
+        result++;
+    }
+
+    let answer75 = document.getElementById("column8-5").value;
+    if(answer75 === "40") {
+        result++;
+    }
+
+    let answer76 = document.getElementById("column8-6").value;
+    if(answer76 === "48") {
+        result++;
+    }
+
+    let answer77 = document.getElementById("column8-7").value;
+    if(answer77 === "56") {
+        result++;
+    }
+
+    let answer78 = document.getElementById("column8-8").value;
+    if(answer78 === "64") {
+        result++;
+    }
+
+    let answer79 = document.getElementById("column8-9").value;
+    if(answer79 === "72") {
+        result++;
+    }
+
+    let answer80 = document.getElementById("column8-10").value;
+    if(answer80 === "80") {
+        result++;
+    }
+
+
+
+
+    let answer81 = document.getElementById("column9-1").value;
+    if(answer81 === "9") {
+        result++;
+    }
+
+    let answer82 = document.getElementById("column9-2").value;
+    if(answer82 === "18") {
+        result++;
+    }
+
+    let answer83 = document.getElementById("column9-3").value;
+    if(answer83 === "27") {
+        result++;
+    }
+
+    let answer84 = document.getElementById("column9-4").value;
+    if(answer84 === "36") {
+        result++;
+    }
+
+    let answer85 = document.getElementById("column9-5").value;
+    if(answer85 === "45") {
+        result++;
+    }
+
+    let answer86 = document.getElementById("column9-6").value;
+    if(answer86 === "54") {
+        result++;
+    }
+
+    let answer87 = document.getElementById("column9-7").value;
+    if(answer87 === "63") {
+        result++;
+    }
+
+    let answer88 = document.getElementById("column9-8").value;
+    if(answer88 === "72") {
+        result++;
+    }
+
+    let answer89 = document.getElementById("column9-9").value;
+    if(answer89 === "81") {
+        result++;
+    }
+
+    let answer90 = document.getElementById("column9-10").value;
+    if(answer90 === "90") {
+        result++;
+    }
+
+
+
+
+    let answer91 = document.getElementById("column10-1").value;
+    if(answer91 === "10") {
+        result++;
+    }
+
+    let answer92 = document.getElementById("column10-2").value;
+    if(answer92 === "20") {
+        result++;
+    }
+
+    let answer93 = document.getElementById("column10-3").value;
+    if(answer93 === "30") {
+        result++;
+    }
+
+    let answer94 = document.getElementById("column10-4").value;
+    if(answer94 === "40") {
+        result++;
+    }
+
+    let answer95 = document.getElementById("column10-5").value;
+    if(answer95 === "50") {
+        result++;
+    }
+
+    let answer96 = document.getElementById("column10-6").value;
+    if(answer96 === "60") {
+        result++;
+    }
+
+    let answer97 = document.getElementById("column10-7").value;
+    if(answer97 === "70") {
+        result++;
+    }
+
+    let answer98 = document.getElementById("column10-8").value;
+    if(answer98 === "80") {
+        result++;
+    }
+
+    let answer99 = document.getElementById("column10-9").value;
+    if(answer99 === "90") {
+        result++;
+    }
+
+    let answer100 = document.getElementById("column10-10").value;
+    if(answer100 === "100") {
+        result++;
+    }
+
+    alert("Кількість правильних відовідей:" + result);
+}
